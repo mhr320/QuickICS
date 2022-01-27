45c7da35a65541481133729d6fa167a3310abc31
mhr320 <mhr320@gmail.com>
2022-01-27 13:47:56 -0500

    uplevel

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(here / 'README.md', 'r') as f:
 
 setup(
     name="QuickICS",
-    version="0.0.1",
+    version="0.0.2",
     author="M. Holbert Roberts",
     author_email="mhr320@gmail.com",
     description="quick way to convert a csv file of events for same type event to .ics file",
