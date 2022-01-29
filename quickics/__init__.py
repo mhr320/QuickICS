a07d56fa2b582aa307f3fa21780fe14a6d88edbd
mhr320 <mhr320@gmail.com>
2022-01-29 12:08:48 -0500

    refactor

--- quickics/__init__.py
+++ quickics/__init__.py
@@ -4,63 +4,70 @@ import csv
 import os
 
 
-class QuickICS:
+class Ics():
 
-    reminder_text = '**Reminder** Single line in csv:\n\n02/06/2022,16:00:00,1:30,\
-Anchor\n\n'
+    rem_text = '*** Reminder ***\n\nIn your .csv file on your Desktop\n\n\
+02/06/2022,16:00:00,1:30,Anchor Avon Lake OH 44012\n\n'
+    ical = []
+    events = []
 
     def __init__(self):
-        self.in_file = input(f'{QuickICS.reminder_text}Enter CSV file -> ')
-        self.name = input('Enter Event Name -> ')
-        self.ical = []
-        self.events = []
-        self.today = datetime.now()
-        self.today = datetime.strftime(self.today, '%d_%b_%y')
-        self.path = os.path.expanduser("~/Desktop")
-        self.FNAME = f"Import_{self.today}"
-        self.in_file = os.path.abspath(os.path.join(self.path, self.in_file))
-        self.save_as = os.path.abspath(os.path.join(self.path, self.FNAME))
-        self.base = os.path.dirname(__file__)
-        self.outfile = f"{self.save_as}.ics"
-        self.loadCSV()
-        self.calendarCreate()
+        self.csv_file = input(f'{Ics.rem_text}Type .csv filename:')
+        self.event_title = input('Events title:')
+        self.today_ = datetime.strftime(datetime.now(), '%d_%b_%y')
+        self.csv_file = os.path.abspath(os.path.join(
+                                        os.path.expanduser("~/Desktop"),
+                                        self.csv_file))
+        self.save_as = os.path.abspath(os.path.join(
+                                        os.path.expanduser("~/Desktop"), 
+                                        f'{self.event_title}_{self.today_}.ics')
+                                       )
+        self.csv_reader(self.csv_file)
+        self.create_calendar()
 
-    def parseDuration(self, time):
-        self.time = time.split(":")
-        self.h = self.time[0]
-        self.m = self.time[1]
-        self.duration = timedelta(hours=int(self.h), minutes=int(self.m))
-        return self.duration
+    def duration(self, time_):
+        self.t = time_.split(":")
+        self.h = self.t[0]
+        self.m = self.t[1]
+        self.dur_ = timedelta(hours=int(self.h), minutes=int(self.m))
+        return self.dur_
 
-    def loadCSV(self):
-        with open(self.in_file, newline='') as f:
+    def csv_reader(self, file):
+        self.file = self.csv_file
+        with open(self.file) as f:
             r = csv.reader(f)
             for i in r:
                 if i != []:
                     self.date_time = f'{i[0]} {i[1]}'
-                    self.dtstamp = datetime.strptime(self.date_time, '%m/%d/%Y %H:%M:%S')  # noqa: E501
-                    self.duration = self.parseDuration(i[2])
+                    self.dtstamp = datetime.strptime(self.date_time, 
+                                                     '%m/%d/%Y %H:%M:%S')
+                    self.duration_ = self.duration(i[2])
                     self.location = vText(i[3])
-                    self.ical.append((self.name, self.dtstamp, self.duration, self.location))  # noqa: E501
+                    Ics.ical.append((
+                            self.event_title,
+                            self.dtstamp,
+                            self.duration_,
+                            self.location
+                        ))
 
-    def calendarCreate(self):
+    def create_calendar(self):
         for x, i in enumerate(self.ical):
-            self.event = Event()
-            self.event.add("summary", i[0])
-            self.event.add("dtstamp", i[1])
-            self.event.add("dtstart", i[1])
-            self.event.add("dtend", i[1] + i[2])
-            self.event.add("location", i[3])
-            self.events.append(self.event)
+            event = Event()
+            event.add("summary", i[0])
+            event.add("dtstamp", i[1])
+            event.add("dtstart", i[1])
+            event.add("dtend", i[1] + i[2])
+            event.add("location", i[3])
+            Ics.events.append(event)
         cal = Calendar()
-        cal.add('prodid', '-//Michael H. Roberts//WMT TO ICS//')
+        cal.add('prodid', '-//Michael H. Roberts//Sports//')
         cal.add('version', '2.0')
         cal.add('calscale', 'GREGORIAN')
-        for evnt in self.events:
+        for evnt in Ics.events:
             cal.add_component(evnt)
-        with open(self.outfile, 'wb') as f:
+        with open(self.save_as, 'wb') as f:
             f.write(cal.to_ical())
 
 
 if __name__ == '__main__':
-    QuickICS()
+    Ics()
